2239dfcd96ca21eebcb96128f55399712e25321e
taddes <taddes.korris@gmail.com>
2021-12-28 18:42:45 -0500

    basic num types

--- Types and Variables/main.rs
+++ Types and Variables/main.rs
@@ -2,6 +2,13 @@
 #[allow(unused_variables)]
 
 fn main() {
-  let a: u8 = 123;
-  println!("a = {}", a)
+  let a: u8 = 123; // u = unsigned, 8 bits, 0 - 255
+  println!("a = {}", a);
+
+  // u = unsigned, 0 to 2^N-1
+  // i = signed,-2^(N-1) .. 2^(N-1) - 1,  -128 -> 127
+  let mut b: i8 = 0; 
+  println!("b = {}", b);
+  b = 42;
+  println!("b = {}", b)
 }
