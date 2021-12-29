fe5d3777637a2153cb48fcd3646a62adf9c5d140
taddes <taddes.korris@gmail.com>
2021-12-29 15:13:57 -0500

    enums and structs

---
+++ Data Structures/main.rs
@@ -0,0 +1,44 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_must_use)]
+
+struct Point {
+  x: f64,
+  y: f64
+}
+
+struct Line {
+  start: Point,
+  end: Point
+}
+
+fn structures() {
+  let p = Point  {x: 3.0, y: 4.0 };
+  println!("point p is at {}, {}", p.x, p.y);
+
+  let p2 = Point { x: 5.0, y: 10.0};
+  let myline = Line { start:p, end:p2 };
+  println!("Line x coords {}, {}", myline.start.x, myline.end.x);
+}
+
+fn enumeration() {
+  enum Color {
+    Red, 
+    Green, 
+    Blue
+  }
+
+  let c:Color = Color::Red;
+
+  match c {
+    Color::Red => println!("r"),
+    Color::Green => println!("g"),
+    Color::Blue => println!("b")
+  }
+
+}
+
+fn main() {
+  // structures();
+  enumeration();
+}
