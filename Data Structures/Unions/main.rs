25781e59bfd53dd26cd7cb6956563590feaaaab0
taddes <taddes.korris@gmail.com>
2021-12-29 16:05:37 -0500

    unions

--- Data Structures/Unions/main.rs
+++ Data Structures/Unions/main.rs
@@ -4,9 +4,25 @@ union IntOrFloat
   f: f32
 }
 
+fn process_value(iof: IntOrFloat) {
+  unsafe {
+    match iof {
+      IntOrFloat { i: 42} => {
+        println!("meaning of life value");
+      }
+      IntOrFloat { f } => {
+        println!("Value is {}", f)
+      }
+    }
+  }
+}
+
 fn main() {
   let mut iof = IntOrFloat { i: 123 };
   iof.i = 234;
 
   let value = unsafe { iof.i };
+
+
+  process_value(IntOrFloat{i:42});
 }
