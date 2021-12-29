89f901db9a35c6db16393c70c336509f3b1e395b
taddes <taddes.korris@gmail.com>
2021-12-28 19:57:15 -0500

    mem module for size

--- Types and Variables/main.rs
+++ Types and Variables/main.rs
@@ -1,6 +1,8 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
 
+use std::mem;
+
 fn main() {
   let a: u8 = 123; // u = unsigned, 8 bits, 0 - 255
   println!("a = {}", a);
@@ -10,5 +12,8 @@ fn main() {
   let mut b: i8 = 0; 
   println!("b = {}", b);
   b = 42;
-  println!("b = {}", b)
+  println!("b = {}", b);
+
+  let c = 123456789; // i32 = 32 bits = 4 bytes
+  println!("c = {}, c takes up {} bytes", c, mem::size_of_val(&c));
 }
