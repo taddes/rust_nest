e15d65bdfd2fb6e5c70a1baa69f4ca68c509e770
taddes <taddes.korris@gmail.com>
2022-01-01 12:13:46 -0500

    exercises

---
+++ exercises/simple_types/target/rls/debug/deps/simple_types-5cdd1dfb815c3148.d
@@ -0,0 +1,5 @@
+c:\Users\Taddes PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\rls\debug\deps\simple_types-5cdd1dfb815c3148.rmeta: src\main.rs
+
+c:\Users\Taddes PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\rls\debug\deps\simple_types-5cdd1dfb815c3148.d: src\main.rs
+
+src\main.rs:
---
+++ exercises/simple_types/target/rls/debug/deps/simple_types-6df6253537e3ccf5.d
@@ -0,0 +1,5 @@
+c:\Users\Taddes PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\rls\debug\deps\simple_types-6df6253537e3ccf5.rmeta: src\main.rs
+
+c:\Users\Taddes PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\rls\debug\deps\simple_types-6df6253537e3ccf5.d: src\main.rs
+
+src\main.rs:
