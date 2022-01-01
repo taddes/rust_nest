8907d338e412ab5431bf5309cd94a7c7204c3f7e
taddes <taddes.korris@gmail.com>
2022-01-01 15:40:14 -0500

    simple types

---
+++ exercises/simple_types/target/debug/deps/simple_types.d
@@ -0,0 +1,5 @@
+C:\Users\Taddes PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\debug\deps\simple_types.exe: src\main.rs
+
+C:\Users\Taddes PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\debug\deps\simple_types.d: src\main.rs
+
+src\main.rs:
---
+++ exercises/simple_types/target/debug/simple_types.d
@@ -0,0 +1,1 @@
+C:\Users\Taddes\ PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\target\debug\simple_types.exe: C:\Users\Taddes\ PC\OneDrive\Sandbox\Rust\rust_nest\exercises\simple_types\src\main.rs
