a0d7e58df176e3f3c3c0fed16bbfcad4a9dc2271
taddes <taddes.korris@gmail.com>
2022-01-13 13:17:18 -0500

    set operations

--- Data Structures/Hash Set/main.rs
+++ Data Structures/Hash Set/main.rs
@@ -2,18 +2,43 @@
 #![allow(unused_imports)]
 #![allow(unused_must_use)]
 
+use std::collections::HashSet;
 use std::thread;
 use std::time;
-use std::collections::HashSet;
 
 fn main() {
-  let mut greeks = HashSet::new();
-  greeks.insert("gamma");
-  greeks.insert("delta");
-  greeks.insert("omicron");
-
-  let added_vega = greeks.insert("vega");
+    let mut greeks = HashSet::new();
+    greeks.insert("gamma");
+    greeks.insert("delta");
+    greeks.insert("omicron");
 
     // Order of insertion does not necessarily match output
     println!("{:?}", greeks);
+
+    // While inserting, this will result in a bool if inserted successfully
+    // If not, False will return (in the case of a duplicate)
+    let added_vega = greeks.insert("vega");
+    if added_vega {
+        println!("added")
+    }
+
+    // Order of insertion does not necessarily match output
+    println!("{:?}", greeks);
+
+    let mut val_to_check = "kappa";
+    if !greeks.contains(&val_to_check) {
+        println!("{} is not in collection", val_to_check);
+    } else {
+        println!("{} is in collection", val_to_check);
+    }
+
+    // Order of insertion does not necessarily match output
+    println!("{:?}", greeks);
+
+    let removed = greeks.remove("delta");
+    println!("{:?}", removed);
+
+    if removed {
+      println!("delta was removed")
+    }
 }
