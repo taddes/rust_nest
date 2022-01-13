66931dd2b1acde620ade863b9f61543f590dac5b
taddes <taddes.korris@gmail.com>
2022-01-13 13:10:17 -0500

    hash set

--- Data Structures/Hash Set/main.rs
+++ Data Structures/Hash Set/main.rs
@@ -7,5 +7,13 @@ use std::time;
 use std::collections::HashSet;
 
 fn main() {
-  
+  let mut greeks = HashSet::new();
+  greeks.insert("gamma");
+  greeks.insert("delta");
+  greeks.insert("omicron");
+
+  let added_vega = greeks.insert("vega");
+
+    // Order of insertion does not necessarily match output
+    println!("{:?}", greeks);
 }
