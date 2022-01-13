b4e24f88df52168b32cc1e2cfb52d3568239425a
taddes <taddes.korris@gmail.com>
2022-01-12 22:40:47 -0500

    hashmap

--- Data Structures/Hash Map/main.rs
+++ Data Structures/Hash Map/main.rs
@@ -4,4 +4,13 @@ fn main() {
   let mut shapes = HashMap::new();
   shapes.insert(String::from("triangle"), 2);
   shapes.insert(String::from("square"), 4);
+
+  println!("a square has {} sides", shapes["square"]);
+  for (key, value) in &shapes {
+    println!("{} {}", key, value);
+  }
+
+  shapes.entry("circle".into()).or_insert(1);
+
+  println!("{:?}", shapes);
 }
