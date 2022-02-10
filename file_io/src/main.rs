5b49b51cb9bfce1597f158099cc7a351fede0fdd
taddes <taddes.korris@gmail.com>
2022-02-10 14:49:19 -0500

    file io

--- file_io/src/main.rs
+++ file_io/src/main.rs
@@ -3,19 +3,28 @@ use std::io::{Error, Read};
 use std::path::PathBuf;
 
 fn read_file_contents(path: PathBuf) -> Result<String, Error> {
-  let mut string = String::new();
-      // Access a file at a specified path
+    let mut string = String::new();
+    // Access a file at a specified path
     // ---------------------------------
     // TODO #1:
     // - Pass variable to `file` variable on success, or
     // - Return from function early if there's an error
     let mut file: File = match File::open(path) {
-      Ok(file_handle) => todo!("Pass variable to file variable on success"), 
-      Err(io_error) => todo!("Return the function early if error")
+        Ok(file_handle) => todo!("Pass variable to file variable on success"),
+        Err(io_error) => todo!("Return the function early if error"),
     };
 
-}
+    // Read file contents into `String` variable with `read_to_string`
+    // ---------------------------------
+    // Success path is already filled in
+    // TODO #2: Return from function early if there's an error
+    match file.read_to_string(&mut string) {
+      Ok(_) => (),
+      Err(io_error) => todo!("Return from function early if error")
+    };
 
+    todo!("Return string variable")
+}
 
 fn main() {
     println!("Hello, world!");
